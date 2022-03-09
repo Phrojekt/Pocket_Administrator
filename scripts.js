b1f31d4b14f4d586551bc84bd146425e847bbb7c
Phrojekt <101226915+Phrojekt@users.noreply.github.com>
2022-03-09 14:17:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -92,7 +92,7 @@ const DOM = {
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img class="button-delete" onclick="Transaction.remove(${index})" src="./assets/total.svg" alt="Remover transação">
+            <img class="button-delete" onclick="Transaction.remove(${index})" src="../assets/total.svg" alt="Remover transação">
         </td>
         `
 
